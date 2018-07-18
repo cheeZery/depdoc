77e4c1a0a02ba95c646b3822b56d386d205ca6cb
GodLesZ <godlesz@hotmail.de>
2018-07-18 06:25:05 +0200

    Nether exit but return the exit code

--- src/Runner.php
+++ src/Runner.php
@@ -1,26 +1,74 @@
 <?php
+declare(strict_types=1);
 
 namespace DepDoc;
-
-// TODO: Add PHP 7.1 as requirement in composer.json
 
 class Runner
 {
     private const ACTIONS = ['validate', 'update'];
 
-    public static function run(array $arguments = [])
+    /** @var Application */
+    private $application;
+
+    public function run(array $arguments = []): int
     {
+        if (count($arguments) && ($arguments[0] === '-h' || $arguments[0] === '--help')) {
+            echo static::getHelp();
+
+            return 0;
+        }
+
         $action = $arguments[0] ?? self::ACTIONS[0];
 
         if (!in_array($action, self::ACTIONS)) {
-            echo "Unrecognized action '$action'!";
-            exit(1);
+            echo "Unrecognized action '$action'!" . PHP_EOL . PHP_EOL;
+            echo static::getHelp();
+
+            return 1;
         }
 
-        $app = new Application();
+        $this->application = new Application();
 
         $actionName = "{$action}Action";
 
-        $app->$actionName();
+        $this->application->$actionName();
+
+        return 0;
+    }
+
+    public function getHelp(): string
+    {
+        return <<<HELP
+Usage:
+  depdoc [command] [options]
+
+Commands:
+  validate      Validate a already generated DEPENDENCIES.md
+  update        Update or create a DEPENDENCIES.md
+
+Options:
+  --help        Output this help 
+
+HELP;
+
+    }
+
+    /**
+     * @return Application
+     */
+    public function getApplication(): Application
+    {
+        return $this->application;
+    }
+
+    /**
+     * @param Application $application
+     * @return $this
+     */
+    public function setApplication(Application $application): Runner
+    {
+        $this->application = $application;
+
+        return $this;
     }
 }
