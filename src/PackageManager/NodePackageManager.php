f821f97c697f95a82b904d15193d05cf07a97850
GodLesZ <godlesz@hotmail.de>
2018-07-26 05:32:59 +0200

    Bugfix node devDependencies are not contain name or description

--- src/PackageManager/NodePackageManager.php
+++ src/PackageManager/NodePackageManager.php
@@ -29,7 +29,7 @@ class NodePackageManager extends AbstractPackageManager
             exit(1);
         }
 
-        $installedPackages = array_merge($dependencies["dependencies"] ?? [], $dependencies['devDependencies'] ?? []);
+        $installedPackages = $dependencies["dependencies"] ?? [];
 
         $relevantData = array_flip(["name", "version", "description"]);
 
